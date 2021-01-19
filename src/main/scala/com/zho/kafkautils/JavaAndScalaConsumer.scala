c8fc9ac4e3053b7b4727e932d1dc36df74a6e16b
zho <7654321>
2021-01-19 18:12:38 +0800

    Update Kafka method

---
+++ src/main/scala/com/zho/kafkautils/JavaAndScalaConsumer.scala
@@ -0,0 +1,15 @@
+package com.zho.kafkautils
+
+object JavaAndScalaConsumer {
+
+}
+
+class JavaAndScalaConsumer {
+
+  def javaConsumer(){}
+
+  def scalaConsumer(): Unit ={
+
+  }
+
+}
---
+++ src/main/scala/com/zho/kafkautils/JavaAndScalaProducer.scala
@@ -0,0 +1,13 @@
+package com.zho.kafkautils
+
+object JavaAndScalaProducer {
+
+}
+
+class JavaAndScalaProducer {
+
+  def javaProducer(){}
+
+  def scalaProducer(){}
+
+}
