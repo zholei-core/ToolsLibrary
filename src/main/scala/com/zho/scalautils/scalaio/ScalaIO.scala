17f9fa0768a7a958fba0858c2e8464b588e39e29
zho <7654321>
2021-01-19 17:02:36 +0800

    Update ScalaCollection method

--- src/main/scala/com/zho/scalautils/scalaio/ScalaIO.scala
+++ src/main/scala/com/zho/scalautils/scalaio/ScalaIO.scala
@@ -13,10 +13,10 @@ object ScalaIO {
     // 获取文件数据里的 所有行 返回数据集合
     val lindata: Iterator[String] = data.getLines()
 
-    // 将文件获取的【数据集合】，直接打印输出
+    // 方案一：将文件获取的【数据集合】，直接打印输出
     lindata.foreach(println(_))
 
-    // 将文件中获取的数据，通过调用 zipWithIndex 转换为 带有行号的数据类型
+    // 方案二：将文件中获取的数据，通过调用 zipWithIndex 转换为 带有行号的数据类型
     //    val dataAndIndex: Iterator[(String, Int)] = lindata.zipWithIndex
     // 将数据 按照 key -> value 类型输出
     //    dataAndIndex.foreach(line => println(line._2 + " -> " + line._1))
