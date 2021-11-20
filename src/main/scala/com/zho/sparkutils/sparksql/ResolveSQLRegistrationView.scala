a548bbf90d883c8d84f2d915916e0b59fb792b37
zhoulei <zholei@aliyun.com>
2021-11-20 15:49:55 +0800

    Update

--- src/main/scala/com/zho/sparkutils/sparksql/ResolveSQLRegistrationView.scala
+++ src/main/scala/com/zho/sparkutils/sparksql/ResolveSQLRegistrationView.scala
@@ -3,6 +3,7 @@ package com.zho.sparkutils.sparksql
 import com.zho.logger.LoggerFactoryUtil
 import org.apache.spark.sql.SparkSession
 
+import scala.collection.mutable.ListBuffer
 import scala.util.matching.Regex
 
 trait ResolveSQLRegistrationView extends LoggerFactoryUtil {
@@ -25,10 +26,12 @@ trait ResolveSQLRegistrationView extends LoggerFactoryUtil {
 
   /**
    *
-   * @param tableMetaList tableMetaList
+   * @param sqlCode sqlCode
    * @param session       session
    */
-  def registerView(tableMetaList: List[String], session: SparkSession): Unit = {
+  def registerView(sqlCode: String, session: SparkSession): (List[String], List[String]) = {
+    val tableMetaList: List[String] = analysisMetabaseAndTableName(sqlCode)
+    val listMeta = ListBuffer.empty[String]
     tableMetaList.foreach(metaElem => {
       //      val tempTable = line.substring(line.indexOf(".") + 1)
       // metabase tableName Split
@@ -45,28 +48,28 @@ trait ResolveSQLRegistrationView extends LoggerFactoryUtil {
         case _ =>
           logger.error(s"Metabase does not exist. Please update the data source configuration : $metaElem")
       }
+      listMeta += databaseTableIter(0)
     })
+    (tableMetaList, listMeta.toList.distinct)
   }
 
-  def registerView(tableMetaList: List[String]): List[String] = {
-    tableMetaList.foreach(metaElem => {
-      //      val tempTable = line.substring(line.indexOf(".") + 1)
+  def registerView(sqlCode: String): (List[String], List[String]) = {
+    val tableMetaList: List[String] = analysisMetabaseAndTableName(sqlCode)
+    val listMeta = ListBuffer.empty[String]
+    tableMetaList.foreach(f = metaElem => {
       // metabase tableName Split
       val databaseTableIter = metaElem.split("\\.")
       databaseTableIter(0) match {
         case "dwd_vw_dev" | "dm_vw_dev" =>
-          //          session
-          //            .read
-          //            .parquet(s"/user/hive/warehouse/${metaElem.split("\\.")(0)}.db/${metaElem.split("\\.")(1)}")
-          //            .createOrReplaceTempView(s"${metaElem.split("\\.")(1)}")
           println(s"register hiveTable view : metadatabase : ${metaElem.split("\\.")(0)} , tableName : ${metaElem.split("\\.")(1)}")
         case "ods" =>
           println(s"register kuduTable view : metadatabase : ${metaElem.split("\\.")(0)} , tableName : ${metaElem.split("\\.")(1)}")
         case _ =>
           logger.error(s"Metabase does not exist. Please update the data source configuration : $metaElem")
       }
+      listMeta += databaseTableIter(0)
     })
-    tableMetaList
+    (tableMetaList, listMeta.toList.distinct)
   }
 
 
